e53801f000cf8e589c035bd45efa2802f8d8b014
Noah Loomans <noahloomans@gmail.com>
2016-05-17 21:27:13 +0200

    Added things

--- script.js
+++ script.js
@@ -27,8 +27,8 @@ var thingsToDo = [
   },
   function () {
     el.header.text('Overheid');
-    el.text.text('De overheid probeerd het beter te maken door hier windmolens te plaatsen,'+
-                'maar wij denken dat het beter kan.');
+    el.text.text('De overheid probeerd het beter te maken door hier windmolens te plaatsen,'
+                 +'maar wij denken dat het beter kan.');
     addMarker({lat: 53, lng: 6}, icons.gas);
     addMarker({lat: 52.75, lng: 5}, icons.windmill);
     addMarker({lat: 52.4, lng: 5.5}, icons.windmill);
@@ -36,13 +36,29 @@ var thingsToDo = [
     addMarker({lat: 53.26, lng: 6.85}, icons.windmill);
   },
   function () {
-    el.header.text('Oplossing')
+    el.header.text('Oplossing');
     el.text.text('Zo zijn er heel veel plantages in nederland. De overheid kan wat van'
-                 +'dit land kopen om dat een heel groot veld gevult met zonnen panelen'
-                 +'plaatsen')
+                +'dit land kopen om dat een heel groot veld gevult met zonnen panelen'
+                +'plaatsen');
     map.setCenter({lat: 52.1356625, lng: 4.8959632});
     map.setMapTypeId(google.maps.MapTypeId.SATELLITE);
     map.setZoom(15);
+  },
+  function () {
+    el.header.text('Oplossing');
+    el.text.html('Verder kunnen we verplicht zonnen panelen zetten op groten'
+                 +'gebouwen in de volgende steden.<br><br>'
+                 +'We hebben de grootste steden gekozen omdat daar ook de grootste'
+                 +'gebouwen zijn en we dus ook het meesten er uit krijgen.');
+    addMarker({lat: 53, lng: 6}, icons.gas);
+    addMarker({lat: 52.75, lng: 5}, icons.windmill);
+    addMarker({lat: 52.4, lng: 5.5}, icons.windmill);
+    addMarker({lat: 51.96, lng: 4.2}, icons.windmill);
+    addMarker({lat: 53.26, lng: 6.85}, icons.windmill);
+    addMarker({lat: 52.36, lng: 4.89}, icons.solarPanel);
+    addMarker({lat: 51.93, lng: 4.48}, icons.solarPanel);
+    addMarker({lat: 52.07, lng: 4.30}, icons.solarPanel);
+    addMarker({lat: 52.09, lng: 5.12}, icons.solarPanel);
   }
 ];
 
@@ -51,9 +67,9 @@ function goToCurrent() {
     markers[i].setMap(null);
   }
   markers = [];
-  map.setCenter({lat: 52, lng: 5});
+  map.setCenter({lat: 52.25, lng: 5});
   map.setMapTypeId(google.maps.MapTypeId.TERRAIN);
-  map.setZoom(7);
+  map.setZoom(8);
   thingsToDo[currentIndex]();
 }
 
@@ -68,12 +84,7 @@ function back() {
 }
 
 function initMap() {
-  window.map = new google.maps.Map(document.getElementById('map'), {
-    // center: {lat: 52.30916308066622, lng: 4.946269362500093},
-    center: {lat: 52, lng: 5},
-    zoom: 7,
-    mapTypeId: google.maps.MapTypeId.TERRAIN
-  });
+  window.map = new google.maps.Map(document.getElementById('map'));
 
   icons.gas = {
     url:'/img/gas.png',
@@ -81,6 +92,10 @@ function initMap() {
   }
   icons.windmill = {
     url: '/img/windmill.png',
+    scaledSize: new google.maps.Size(50,50)
+  }
+  icons.solarPanel = {
+    url: '/img/solar panel.png',
     scaledSize: new google.maps.Size(50,50)
   }
 
